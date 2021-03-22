6c732a1c4c9af9881d90b69a4212e3785aa4801e
kunalkale98 <kunal,k2898@gmail.com>
2021-03-23 02:11:37 +0530

    Added: Method to calculate Aggregate Total Fare

--- src/main/java/com/invoice/InvoiceGenerator.java
+++ src/main/java/com/invoice/InvoiceGenerator.java
@@ -1,10 +1,26 @@
 package com.invoice;
 
+import java.util.List;
+
 public class InvoiceGenerator {
+    private static final int COST_PER_KM = 10;
+    private static final int COST_PER_MIN = 1;
+    private static final int MIN_FARE = 5;
+
     public int calculateFare(int distance,int time){
-        int minimumFare = 5;
-        int totalFare = 10*distance + time + minimumFare;
+        int fare = Math.max(COST_PER_KM*distance + COST_PER_MIN*time , MIN_FARE);
+        System.out.println("Fare: "+fare);
+        return fare;
+    }
+
+    public int totalFare(List<Ride> rideList){
+        int totalFare = 0;
+        for (Ride ride: rideList) {
+            totalFare += calculateFare(ride.distance,ride.time);
+        }
         System.out.println("Total Fare: "+totalFare);
         return totalFare;
     }
+
+
 }
--- src/test/java/com/invoice/InvoiceTest.java
+++ src/test/java/com/invoice/InvoiceTest.java
@@ -4,18 +4,34 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InvoiceTest {
     InvoiceGenerator invoice;
+    public List<Ride> rideList;
     Ride ride1 = new Ride(10,15);
+    Ride ride2 = new Ride(5,10);
+    Ride ride3 = new Ride(7,12);
 
     @Before
     public void setup(){
         invoice = new InvoiceGenerator();
+        rideList = new ArrayList<>();
+        rideList.add(ride1);
+        rideList.add(ride2);
+        rideList.add(ride3);
     }
 
     @Test
-    public void givenData_ToFindTotalFare_ShouldReturnFare() {
-        int totalFare = invoice.calculateFare(ride1.distance,ride1.time);
-        Assert.assertEquals(totalFare,120);
+    public void givenData_ToCalculateFare_ShouldReturnFare() {
+        int fare = invoice.calculateFare(ride1.distance,ride1.time);
+        Assert.assertEquals(fare,120);
+    }
+
+    @Test
+    public void givenData_ToFindTotalFare_ShouldReturnTotalFare() {
+        int totalFare = invoice.totalFare(rideList);
+        Assert.assertEquals(257,totalFare);
     }
 }
